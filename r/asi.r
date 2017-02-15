025e1aded536edf356d77647b1297143f3ceea23
Wlodek Bzyl <matwb@univ.gda.pl>
2017-02-15 11:23:55 +0100

    update r/asi.r

--- r/asi.r
+++ r/asi.r
@@ -1,9 +1,10 @@
 #! /usr/bin/env Rscript
 
-args <- commandArgs(TRUE)
+# args <- commandArgs(TRUE)
 
-args[1] = "~/Downloads/lZaj_Architektura_serwisow_internetowych_1_Architektura_serwisow_internetowych_20162_Informatyka_L.xls"
-filename = args[1]
+# filename = args[1]
+filename = "~/Downloads/lZaj_Architektura_serwisow_internetowych_1_Architektura_serwisow_internetowych_20162_Informatyka_L.xls"
+# filename = "~/Dropbox/Instytut/ASI/asi_2017.csv"
 
 require(readxl)
 require(readr)
@@ -25,17 +26,43 @@ extra = tribble(
   "Osowska", "Aleksandra", 235260,
   "Pyszyńska", "Martyna", 235307,
   "Banaszek", "Dawid", 235385,
-  "Jędzierowska", "Magdalena", 235256
+  "Jędzierowska", "Magdalena", 235256,
+  "Szałwicki","Robert",235347
 )
 
-rbind(asi, extra) %>% mutate(nr = row_number())
-
-asi = asi %>%
+asi = rbind(asi, extra) %>%
   arrange(last_name, first_name) %>%
-  mutate(id = as.integer(id), nr = 1:nrow(asi)) %>%
+  mutate(nr = row_number(), id = as.integer(id)) %>%
   select(c(4, 2, 3, 1))
 
 write_csv(asi, paste0(tools::file_path_sans_ext(filename), '.csv'))
 
-# options(tibble.print_max = 61)
-# asi
+## manually insert groups
+
+options(tibble.print_max = 62)
+
+filename = "~/Dropbox/Instytut/ASI/asi_2017.csv"
+all = read_csv(filename) %>%
+  arrange(last_name, first_name) %>%
+  mutate(nr = row_number()) %>%
+  select(nr, last_name, first_name, id, group)
+
+write_csv(all, paste0(tools::file_path_sans_ext(filename), '-all.csv'))
+
+by_group = all %>%
+  arrange(group)
+
+by_group
+
+write_csv(by_group, '~/Dropbox/Instytut/ASI/by_group.csv'))
+
+by_group %>%
+  group_by(group) %>%
+  summarise(total = n())
+
+#   group total
+# -------------
+# 1     1    12
+# 2     2    27
+# 3     3     9
+# 4    NA    14
